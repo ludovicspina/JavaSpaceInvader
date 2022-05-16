8f48e3d001ccf79e0aea7ea5e953bb81f7adb7a0
Ludovic Spina <74927851+ludovicspina@users.noreply.github.com>
2022-05-16 15:07:56 +0200

    Editing levels

--- src/game/Spawn.java
+++ src/game/Spawn.java
@@ -13,6 +13,7 @@ public class Spawn {
     private Random r;
     private boolean isBossSpawned = false;
     private BossEnemy bob = null;
+    private BossEnemy alfred = null;
     public static int difficultyLevel = 1;
 
     public Spawn(Handler handler, Health health) {
@@ -25,33 +26,49 @@ public class Spawn {
 
         if (keepScore == -1) {
             handler.add(new Player(Game.WIDTH / 2 - 32, Game.HEIGHT / 2 - 32, ID.Player, handler));
-            handler.add(new BasicEnemy(r.nextInt(Game.WIDTH - 16), r.nextInt(Game.HEIGHT - 16), ID.Enemy));
         }
         keepScore++;
 
-        if (Health.HEALTH <= 0) {
-            Game.gameState = ID.Over;
-            Over.save();
-            Over.pseudoSave();
-        }
+
+
+
+        // basic enemy : handler.add(new BasicEnemy(r.nextInt(Game.WIDTH - 16), r.nextInt(Game.HEIGHT - 16), ID.Enemy));
+        // smart enemy : handler.add(new SmartEnemy(50, 50, ID.SmartEnemy, handler));
+
 
         if (keepScore >= 100) {
             keepScore = 0;
             health.setLevel(health.getLevel() + 1);
 
             if (difficultyLevel == 1) {
-                if (health.getLevel() > 1) {
-                    handler.add(new SmartEnemy(50, 50, ID.SmartEnemy, handler));
+                if (health.getLevel() >= 1) {
+                    handler.add(new BasicEnemy(r.nextInt(Game.WIDTH - 16), r.nextInt(Game.HEIGHT - 16), ID.Enemy));
                 }
+
+                if (health.getLevel() > 5) {
+                    handler.add(new SmartEnemy(r.nextInt(Game.WIDTH - 16), r.nextInt(Game.HEIGHT - 16), ID.SmartEnemy, handler));
+                }
+
+                if (health.getLevel() >= 10) {
+                    if (!isBossSpawned) {
+                        bob = new BossEnemy(Game.WIDTH / 2, Game.HEIGHT - 40, ID.BossEnemy);
+                        handler.objects.add(bob);
+                        isBossSpawned = true;
+                    }
+                    handler.add(new ProjBossEnnemy(bob.getX(), bob.getY(), ID.ProjBossEnemy));
+                }
+
             }
 
             if (difficultyLevel == 2) {
-                if (health.getLevel() > 1) {
-                    handler.add(new SmartEnemy(300, 300, ID.SmartEnemy, handler));
+                if (health.getLevel() >= 1) {
+                    handler.add(new BasicEnemy(r.nextInt(Game.WIDTH - 16), r.nextInt(Game.HEIGHT - 16), ID.Enemy));
+                    handler.add(new BasicEnemy(r.nextInt(Game.WIDTH - 16), r.nextInt(Game.HEIGHT - 16), ID.Enemy));
                 }
 
-                if (health.getLevel() > 1) {
-                    handler.add(new SmartEnemy(50, 50, ID.SmartEnemy, handler));
+                if (health.getLevel() > 5) {
+                    handler.add(new SmartEnemy(r.nextInt(Game.WIDTH - 16), r.nextInt(Game.HEIGHT - 16), ID.SmartEnemy, handler));
+                    handler.add(new SmartEnemy(r.nextInt(Game.WIDTH - 16), r.nextInt(Game.HEIGHT - 16), ID.SmartEnemy, handler));
                 }
 
                 if (health.getLevel() >= 10) {
